01bc26482a35ce72dcb3446d9b9456acb87e7a6f
Tim <github@timgreen.ws>
2026-07-27 11:52:33 +1000

    added theme setup

--- functions.php
+++ functions.php
@@ -3,6 +3,24 @@
 /**
  * Shortcuts functions and definitions
  */
+// Theme setup
+function shortcuts_setup()
+{
+	add_theme_support('title-tag');
+	add_theme_support('post-thumbnails');
+	add_theme_support('html5', ['search-form', 'comment-form', 'comment-list', 'gallery', 'caption']);
+	add_theme_support('custom-logo', [
+		'height'      => 40,
+		'width'       => 120,
+		'flex-height' => true,
+		'flex-width'  => true,
+	]);
+
+	register_nav_menus([
+		'primary'      => __('Primary Menu', 'shortcuts'),
+		'footer-bottom' => __('Footer Bottom Links', 'shortcuts'),
+	]);
+
 // Enqueue scripts and styles
 function shortcuts_enqueue_assets() {
     $theme_dir = get_template_directory();
