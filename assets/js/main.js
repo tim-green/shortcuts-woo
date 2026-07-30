8b9a649c6dc07a6e21bd58a78b67c9b1c7e8e23d
Tim <github@timgreen.ws>
2026-07-30 17:42:45 +1000

    added fontsource

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,4 +2,19 @@
 import '../sass/app.scss';
 
 // JS Here
-import '../js/app.js';
+import '../js/app.js';// FontSource Here
+import '@fontsource/inter/400.css';
+import '@fontsource/inter/500.css';
+import '@fontsource/inter/600.css';
+import '@fontsource/inter/700.css';
+import '@fontsource/inter/800.css';
+import '@fontsource/noto-sans/100.css';
+import '@fontsource/noto-sans/200.css';
+import '@fontsource/noto-sans/300.css';
+import '@fontsource/noto-sans/400.css';
+import '@fontsource/noto-sans/500.css';
+import '@fontsource/noto-sans/600.css';
+import '@fontsource/noto-sans/700.css';
+import '@fontsource/noto-sans/800.css';
+import '@fontsource/noto-sans/900.css';
+import '@fontsource/material-icons-outlined';
