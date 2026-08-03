ddc6b25a1353f2416a2648c556c5f845c29fe375
Tim <github@timgreen.ws>
2026-08-03 22:26:56 +1000

    added archive template

---
+++ archive.php
@@ -0,0 +1,76 @@
+<?php get_header(); ?>
+
+<section class="page-section" data-section="Archive">
+	<?php shortcuts_page_banner(); ?>
+	<div class="container">
+
+		<?php if (have_posts()) : ?>
+		<div class="blog-layout">
+			<div class="blog-main">
+				<div class="articles-grid">
+					<?php
+					$article_index = 0;
+					while (have_posts()) :
+						the_post();
+						$is_featured = ($article_index === 0);
+					?>
+						<article class="article-card <?php echo $is_featured ? 'article-card--featured' : ''; ?>">
+							<?php if ($is_featured) : ?>
+								<div class="article-image">
+										<?php 
+										if (has_post_thumbnail()) : 
+											the_post_thumbnail('medium_large'); 
+										else : 
+										?>
+										<svg xmlns="http://www.w3.org/2000/svg" height="64" width="64" viewBox="0 0 640 640"><path opacity="0.94" fill="rgb(101, 116, 139)" d="M512 144C520.8 144 528 151.2 528 160L528 416C528 424.8 520.8 432 512 432L192 432C183.2 432 176 424.8 176 416L176 160C176 151.2 183.2 144 192 144L512 144zM192 96C156.7 96 128 124.7 128 160L128 416C128 451.3 156.7 480 192 480L512 480C547.3 480 576 451.3 576 416L576 160C576 124.7 547.3 96 512 96L192 96zM272 208C272 190.3 257.7 176 240 176C222.3 176 208 190.3 208 208C208 225.7 222.3 240 240 240C257.7 240 272 225.7 272 208zM412.7 211.8C408.4 204.5 400.5 200 392 200C383.5 200 375.6 204.5 371.3 211.8L324.8 290.8L307.6 266.2C303.1 259.8 295.8 256 287.9 256C280 256 272.7 259.8 268.2 266.2L212.2 346.2C207.1 353.5 206.4 363.1 210.6 371C214.8 378.9 223.1 384 232 384L472 384C480.6 384 488.6 379.4 492.8 371.9C497 364.4 497 355.2 492.7 347.8L412.7 211.8zM80 216C80 202.7 69.3 192 56 192C42.7 192 32 202.7 32 216L32 512C32 547.3 60.7 576 96 576L456 576C469.3 576 480 565.3 480 552C480 538.7 469.3 528 456 528L96 528C87.2 528 80 520.8 80 512L80 216z"/></svg>
+									<?php endif; ?>
+								</div>
+							<?php endif; ?>
+							<div class="article-body">
+								<div class="article-meta">
+									<?php
+									$categories = get_the_category();
+									if (!empty($categories)) :
+									?>
+										<span class="tag"><?php echo esc_html($categories[0]->name); ?></span>
+										<span class="date"><?php echo get_the_date('F j, Y'); ?></span>
+
+									<?php endif; ?>
+								</div>
+								<h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+								
+								<p><?php echo wp_trim_words(get_the_excerpt(), 20, '...'); ?></p>
+								<a href="<?php the_permalink(); ?>" class="article-link"><?php esc_html_e('Read More', 'shortcuts'); ?>
+									<svg width="14" height="14" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2.5" stroke-linecap="round" stroke-linejoin="round"><path d="M5 12h14"/><path d="m12 5 7 7-7 7"/></svg>
+								</a>
+							</div>
+						</article>
+					<?php
+					$article_index++;
+					endwhile;
+					?>
+				</div>
+
+				<div class="pagination">
+					<?php the_posts_pagination([
+						'mid_size'  => 2,
+						'prev_text' => __('&laquo; Previous', 'shortcuts'),
+						'next_text' => __('Next &raquo;', 'shortcuts'),
+					]);
+					?>
+				</div>
+			</div>
+
+			<?php get_sidebar(); ?>
+		</div>
+
+		<?php else : ?>
+			<div class="content-card">
+				<p><?php esc_html_e('No posts found.', 'shortcuts'); ?></p>
+				<a href="<?php echo esc_url(home_url('/')); ?>" class="btn btn-primary"><?php esc_html_e('Back to Home', 'shortcuts'); ?></a>
+			</div>
+		<?php endif; ?>
+	</div>
+</section>
+
+<?php get_footer(); ?>
