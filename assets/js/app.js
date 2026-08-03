ecb27f2adf43acc50f680dc2b4e538aa0e395fc7
Tim <github@timgreen.ws>
2026-08-03 22:27:39 +1000

    added back to top

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,3 +6,22 @@ const $ = window.jQuery;
 window.$ = $;
 window.jQuery = $;
 
+// Back to Top
+(function () {
+  var btn = document.getElementById('back-to-top');
+  if (!btn) return;
+
+  var onScroll = function () {
+    if (window.scrollY > 400) {
+      btn.classList.add('visible');
+    } else {
+      btn.classList.remove('visible');
+    }
+  };
+
+  window.addEventListener('scroll', onScroll, { passive: true });
+
+  btn.addEventListener('click', function () {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  });
+})();
