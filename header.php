ad22c7bef7431420d9fa6be6136f21fbe20a84c6
Tim <github@timgreen.ws>
2026-08-31 16:06:26 +1000

    added <headeR>

--- header.php
+++ header.php
@@ -12,6 +12,7 @@
 </head>
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
+<header data-section="Header">
       <nav>
         <?php
         wp_nav_menu([
@@ -35,3 +36,4 @@
           <button type="submit" class="search-overlay-submit"><?php esc_html_e('Search', 'shortcuts'); ?></button>
         </div>
       </form>
+</header>
